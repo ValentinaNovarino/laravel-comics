af506270032a802d86aba87d156935958dafac9f
ValentinaNovarino <valenovarino@gmail.com>
2021-01-07 17:21:10 +0100

    stilato header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,55 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <a class="uppercase" href="#">dc power℠ visa®</a>
+            <a class="uppercase" href="#">additional dc sites ▾</a>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="{{ asset('images/dc-logo.png')}}" alt="">
+                </div>
+                <div class="header-menu">
+                    <ul>
+                        <li>
+                            <a class="uppercase" href="#">characters</a>
+                        </li>
+                        <li>
+                            <a class="uppercase active" href="#">comics</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">movies</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">tv</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">games</a>
+                        </li>
+                        <li>
+                            <a  class="uppercase" href="#">collectibles</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">videos</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">fans</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">news</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">shop</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
