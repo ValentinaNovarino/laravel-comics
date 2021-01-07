b231f23523ed502914799e05f1b59e019fa0dbac
ValentinaNovarino <valenovarino@gmail.com>
2021-01-07 18:16:53 +0100

    stilato footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,119 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="footer-menu-left">
+                <h2 class="uppercase">dc comics</h2>
+                <ul>
+                    <li>
+                        <a href="#">Character</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+                <h2 class="uppercase">shop</h2>
+                <ul>
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-menu-center">
+                <h2 class="uppercase">dc</h2>
+                <ul>
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshop</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-menu-right">
+                <h2 class="uppercase">sites</h2>
+                <ul>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="sign-up">
+                    <button class="uppercase" type="button" name="button">
+                        <a href="#">sign-up now!</a>
+                    </button>
+                </div>
+                <div class="social">
+                    <span class="uppercase">follow us</span>
+                    <img src="{{ asset('images/footer-facebook.png')}}" alt="facebook-icon">
+                    <img src="{{ asset('images/footer-twitter.png')}}" alt="twitter-icon">
+                    <img src="{{ asset('images/footer-youtube.png')}}" alt="youtube-icon">
+                    <img src="{{ asset('images/footer-pinterest.png')}}" alt="pinterest-icon">
+                    <img src="{{ asset('images/footer-periscope.png')}}" alt="periscope-icon">
+
+                </div>
+
+            </div>
+            </div>
+        </div>
+    </div>
+</footer>
