87d6ef206c087ca645ac87c1fbae1403065651b6
ValentinaNovarino <valenovarino@gmail.com>
2021-01-08 18:44:29 +0100

    stilato layout card comic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        @foreach ($series as $element)
-            <div class="card">
-                <img src="{{ $element['thumb']}}" alt="{{ $element['series']}}">
-                <span>{{ $element['series']}}</span>
+    <section id="series">
+        <div class="container">
+            <div class="section-title">
+                <h2 class="uppercase">curent series</h2>
             </div>
-        @endforeach
-    </div>
+            <div class="card-container">
+                @foreach ($series as $element)
+                    <div class="card">
+                        <div class="card-img">
+                            <img src="{{ $element['thumb']}}" alt="{{ $element['series']}}">
+                        </div>
+                        <div class="card-title">
+                            <span class="uppercase">{{ $element['series']}}</span>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
