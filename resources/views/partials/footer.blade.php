620a7608cebace3e849efc55a41686bd6f228ec8
ValentinaNovarino <valenovarino@gmail.com>
2021-01-08 18:13:19 +0100

    modifica icone footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,98 +1,100 @@
 <footer>
     <div class="footer-top">
         <div class="container">
-            <div class="footer-menu-left">
-                <h2 class="uppercase">dc comics</h2>
-                <ul>
-                    <li>
-                        <a href="#">Character</a>
-                    </li>
-                    <li>
-                        <a href="#">Comics</a>
-                    </li>
-                    <li>
-                        <a href="#">Movies</a>
-                    </li>
-                    <li>
-                        <a href="#">tv</a>
-                    </li>
-                    <li>
-                        <a href="#">Games</a>
-                    </li>
-                    <li>
-                        <a href="#">Videos</a>
-                    </li>
-                    <li>
-                        <a href="#">News</a>
-                    </li>
-                </ul>
-                <h2 class="uppercase">shop</h2>
-                <ul>
-                    <li>
-                        <a href="#">Shop DC</a>
-                    </li>
-                    <li>
-                        <a href="#">Shop DC Collectibles</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="footer-menu-center">
-                <h2 class="uppercase">dc</h2>
-                <ul>
-                    <li>
-                        <a href="#">Terms Of Use</a>
-                    </li>
-                    <li>
-                        <a href="#">Privacy Policy (New)</a>
-                    </li>
-                    <li>
-                        <a href="#">Ad Choices</a>
-                    </li>
-                    <li>
-                        <a href="#">Advertising</a>
-                    </li>
-                    <li>
-                        <a href="#">Jobs</a>
-                    </li>
-                    <li>
-                        <a href="#">Subscriptions</a>
-                    </li>
-                    <li>
-                        <a href="#">Talent Workshop</a>
-                    </li>
-                    <li>
-                        <a href="#">CPSC Certificates</a>
-                    </li>
-                    <li>
-                        <a href="#">Ratings</a>
-                    </li>
-                    <li>
-                        <a href="#">Shop Help</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="footer-menu-right">
-                <h2 class="uppercase">sites</h2>
-                <ul>
-                    <li>
-                        <a href="#">DC</a>
-                    </li>
-                    <li>
-                        <a href="#">MAD Magazine</a>
-                    </li>
-                    <li>
-                        <a href="#">DC Kids</a>
-                    </li>
-                    <li>
-                        <a href="#">DC Universe</a>
-                    </li>
-                    <li>
-                        <a href="#">DC Power Visa</a>
-                    </li>
-                </ul>
+            <div class="footer-menu">
+                <div class="footer-menu-left">
+                    <h2 class="uppercase">dc comics</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Character</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">tv</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                    <h2 class="uppercase">shop</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-menu-center">
+                    <h2 class="uppercase">dc</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Terms Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy (New)</a>
+                        </li>
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="#">Talent Workshop</a>
+                        </li>
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-menu-right">
+                    <h2 class="uppercase">sites</h2>
+                    <ul>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
         <div class="footer-bottom">
@@ -104,11 +106,23 @@
                 </div>
                 <div class="social">
                     <span class="uppercase">follow us</span>
-                    <img src="{{ asset('images/footer-facebook.png')}}" alt="facebook-icon">
-                    <img src="{{ asset('images/footer-twitter.png')}}" alt="twitter-icon">
-                    <img src="{{ asset('images/footer-youtube.png')}}" alt="youtube-icon">
-                    <img src="{{ asset('images/footer-pinterest.png')}}" alt="pinterest-icon">
-                    <img src="{{ asset('images/footer-periscope.png')}}" alt="periscope-icon">
+                    <ul>
+                        <li class="facebook">
+                            <a href="#"></a>
+                        </li>
+                        <li class="twitter">
+                            <a href="#"></a>
+                        </li>
+                        <li class="youtube">
+                            <a href="#"></a>
+                        </li>
+                        <li class="pinterest">
+                            <a href="#"></a>
+                        </li>
+                        <li class="periscope">
+                            <a href="#"></a>
+                        </li>
+                    </ul>
 
                 </div>
 
