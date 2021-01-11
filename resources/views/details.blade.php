d52e652026ad90e26bb33b14fe02938b37d03787
ValentinaNovarino <valenovarino@gmail.com>
2021-01-11 09:27:10 +0100

    prima parte pagina dettaglio

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -13,4 +13,33 @@
             </div>
         </div>
     </section>
+    <section id="info">
+        <div class="container-xs">
+            <div class="comic-info">
+                <h1 class="uppercase">{{ $comic['title'] }}</h1>
+                <div class="availability-container">
+                    <div class="price">
+                        <span>U.S. Price:</span>
+                        <span class="comic-price">{{ $comic['price'] }}</span>
+                    </div>
+                    <div class="availability">
+                        <span class="comic-availability uppercase">Available</span>
+                    </div>
+                    <div class="check">
+                        <span>Check Avalibality</span>
+                    </div>
+                </div>
+                <div class="description-container">
+                    <p class="comic-description">
+                        {{ $comic['description'] }}
+                    </p>
+                </div>
+            </div>
+            <div class="comic-adv">
+                <h5 class="uppercase">Advertisement</h5>
+                <img src="{{ asset('images/adv.jpg') }}" alt="adv">
+
+            </div>
+        </div>
+    </section>
 @endsection
